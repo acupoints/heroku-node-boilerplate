99f55c3090f5c15892ee49044df65fe248d96554
Gullies <1448991007@qq.com>
2020-07-05 20:08:00 +0800

    temp05

--- index.js
+++ index.js
@@ -1,12 +1,40 @@
 const express = require('express')
 const app = express()
+const bodyparser = require('body-parser')
 
-const port = process.env.PORT || 3000
+app.set('port', process.env.PORT || 3000)
 
-app.get('/', (req, res) => {
-    res.send('Hello heroku-node-boilerplate')
+const articles = [{title: 'Example'}]
+
+app.use(bodyparser.json())
+app.use(bodyparser.urlencoded({extended: true}))
+
+app.get('/articles', (req, res, next) => {
+    res.send(articles)
 })
 
-app.listen(port, () => {
-    console.log(`Express web app available at localhost: ${port}`)
+app.post('/articles', (req, res, next) => {
+    // res.send('OK')
+    const article = {title: req.body.title}
+    articles.push(article)
+    res.send(article)
 })
+
+app.get('/articles/:id', (req, res, next) => {
+    const id = req.params.id
+    console.log('Fetching:', id)
+    res.send(articles[id])
+})
+
+app.delete('/articles/:id', (req, res, next) => {
+    const id = req.params.id
+    console.log('Deleting:', id)
+    delete articles[id]
+    res.send(articles[id])
+})
+
+app.listen(app.get('port'), () => {
+    console.log('App started on port', app.get('port'))
+})
+
+module.exports = app
