071716cfc104d4943994b02fafbc98fb5e4e23c0
Gullies <1448991007@qq.com>
2020-07-05 19:36:26 +0800

    temp4

--- index.js
+++ index.js
@@ -1,42 +1,12 @@
-const cool = require('cool-ascii-faces')
-const {Pool} = require('pg')
-const pool = new Pool({
-    connectionString: process.env.DATABASE_URL,
-    ssl: {
-        rejectUnauthorized: false
-    }
-});
 const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const app = express()
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => res.send(showTimes()))
-  .get('/db', async (req, res) => {
-      try {
-          const client = await pool.connect();
-          const result = await client.query('select * from test_table');
-          const results = {'results': (result) ? result.rows : null};
-          res.render('pages/db', results);
-          client.release();
-      } catch (err) {
-          console.error(err);
-          res.send("Error " + err);
-      }
-  })
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const port = process.env.PORT || 3000
 
+app.get('/', (req, res) => {
+    res.send('Hello heroku-node-boilerplate')
+})
 
-  showTimes = () => {
-    let result = '';
-    const times = process.env.TIMES || 5;
-    for (i = 0; i < times; i++) {
-      result += i + ' ';
-    }
-    return result;
-  }
+app.listen(port, () => {
+    console.log(`Express web app available at localhost: ${port}`)
+})
