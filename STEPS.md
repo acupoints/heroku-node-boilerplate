724a1ff74d3407660ad8d3c482faadd5d76762f8
Gullies <1448991007@qq.com>
2020-07-06 21:58:53 +0800

    node index.js

--- STEPS.md
+++ STEPS.md
@@ -1,6 +1,10 @@
 # STEPS
 
-```bash
+```sh
 npm install
+npm start
 
 ```
+
+## Open in browser
+[node-js-getting-started](https://red-drake-78755.herokuapp.com/)
--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(
     )
     
 app.get('/', (req, res) => {
-    res.send('Hello2')
+    res.send('Hello3')
 })
 app.get('/articles', (req, res, next) => {
     // res.send(articles)
