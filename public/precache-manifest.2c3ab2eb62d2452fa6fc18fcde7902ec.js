74aeff384179fe93683c3ee49324f9d765d6ec86
Artemisia <lanbinzheng@qq.com>
2020-07-24 14:35:07 +0800

    Generate merge project template

---
+++ public/precache-manifest.2c3ab2eb62d2452fa6fc18fcde7902ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7aabcfaf89da9c07ee400e31fa83830e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d5792fc9cd3ca778a74a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aefafbd2e88cec03a2e3",
+    "url": "/static/js/2.8b66dbd9.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8b66dbd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d5792fc9cd3ca778a74a",
+    "url": "/static/js/main.96c105ad.chunk.js"
+  },
+  {
+    "revision": "c21438550d3812e8d482",
+    "url": "/static/js/runtime-main.32448414.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
